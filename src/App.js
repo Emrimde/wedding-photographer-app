81060b3c217a3791c939c06cfa86d3001a68d8b0
unknown <kkrzysztofgargas@gmail.com>
2024-03-29 10:44:01 +0100

    Created button component and imported Bootstrap

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
+import Button from './components/Button.tsx';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      
+        <Button customClass = "btn-success">Click me</Button>
     </div>
   );
 }
