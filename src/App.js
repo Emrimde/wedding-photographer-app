686a26399ab18a42f33df68f150bb2681c713bc0
unknown <kkrzysztofgargas@gmail.com>
2024-04-03 22:54:49 +0200

    Refactored code

--- src/App.js
+++ src/App.js
@@ -1,14 +1,13 @@
-import Button from './Components/Button/Button.tsx';
-import Link from './Components/Link/Link.tsx';
 import './App.css';
 import Nav from './Components/Nav/Nav.tsx';
 import Section from './Components/Section/Section.tsx';
 
+
 function App() {
   return (
     <div className="App">
-        <Nav/>
-        <Section customClass="landing-page"/>
+      <Nav />
+      <Section customClass="landing-page"> </Section>
     </div>
   );
 }
