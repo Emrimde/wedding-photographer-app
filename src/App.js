5131f18a9f27aa6d8125ca14d840a766f56633bf
unknown <kkrzysztofgargas@gmail.com>
2024-04-04 14:18:07 +0200

    Created Div component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
 import './App.css';
 import Nav from './Components/Nav/Nav.tsx';
 import Section from './Components/Section/Section.tsx';
+import Link from './Components/Link/Link.tsx';
+import Div from './Components/Div/Div.tsx';
 
 
 function App() {
   return (
     <div className="App">
       <Nav />
-      <Section customClass="landing-page"> </Section>
+      <Section customClass="landing-page ">
+        <Div customClass="normal-div">
+          <Link href="#" customClass=" scrolling-down-link ">V</Link>
+        </Div>
+      </Section>
     </div>
   );
 }
--- src/Components/Section/Section.tsx
+++ src/Components/Section/Section.tsx
@@ -1,13 +1,17 @@
-import React from "react"
+import React, { ReactNode } from "react"
+import Link from "../Link/Link.tsx"
 import "./Section.css"
 
 type SectionProps = {
+    children: ReactNode,
     customClass: string,
 }
 
-function Section(props : SectionProps){
-    return(
-        <section className={props.customClass}></section>
+function Section(props: SectionProps) {
+    return (
+        <section className={props.customClass}>
+           {props.children}
+        </section>
     )
 }
 
