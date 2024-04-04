6fe6735597ca9ebaa5bf692a16af64bda6b9e02e
unknown <kkrzysztofgargas@gmail.com>
2024-04-04 15:58:17 +0200

    Created second section component

--- src/App.js
+++ src/App.js
@@ -3,17 +3,20 @@ import Nav from './Components/Nav/Nav.tsx';
 import Section from './Components/Section/Section.tsx';
 import Link from './Components/Link/Link.tsx';
 import Div from './Components/Div/Div.tsx';
+import Header from './Components/Header/Header.tsx';
+import SecondSection from './Components/Second-Section/Second-Section.tsx';
 
 
 function App() {
   return (
     <div className="App">
       <Nav />
-      <Section customClass="landing-page ">
-        <Div customClass="normal-div">
-          <Link href="#" customClass=" scrolling-down-link ">V</Link>
-        </Div>
+      <Section customClass="landing-page">
+         <Div customClass="header-div">
+          <Header customClass="header-name text-center">Photographer's Name</Header>
+          </Div> 
       </Section>
+      <SecondSection></SecondSection>
     </div>
   );
 }
