a024ce955a8a042c4baa17d880fde5786ee96d1d
unknown <kkrzysztofgargas@gmail.com>
2024-05-29 10:47:09 +0200

    Nav component refactored

--- src/Components/Nav/Nav.tsx
+++ src/Components/Nav/Nav.tsx
@@ -1,4 +1,4 @@
-import React, { ReactNode } from "react"
+import React from "react"
 import "./Nav.css";
 import Link from "../Link/Link.tsx";
 
@@ -9,12 +9,10 @@ function Navv() {
                 <div className="container py-3">
                     <div className="row">
                         <div className=" col-4 col-sm-4">
-                            <h1 className="d-none d-sm-block"> <a href="/">Homepage</a></h1>
+                            <h1 className="d-none d-sm-block"><Link customClass="" href="/" target="">Homepage</Link></h1>
                         </div>
-
                         <div className=" col-12 col-sm-8">
                             <div className="h-100 d-flex justify-content-evenly align-items-center">
-
                                 <Link href="/about-me" customClass="fs-4 d-none d-lg-block link-scale" target="">About me</Link>
                                 <Link href="/offer" customClass="fs-4 d-none d-lg-block link-scale" target="">Offer</Link>
                                 <Link href="/contact" customClass="fs-4 d-none d-lg-block  link-scale" target="">Contact</Link>
@@ -24,11 +22,11 @@ function Navv() {
                                         Menu
                                     </button>
                                     <ul className="dropdown-menu">
-                                        <li><a className="dropdown-item d-block d-sm-none" href="/">Homepage</a></li>
-                                        <li><a className="dropdown-item" href="/about-me">About me</a></li>
-                                        <li><a className="dropdown-item" href="/offer">Offer</a></li>
-                                        <li><a className="dropdown-item" href="/contact">Contact</a></li>
-                                        <li><a className="dropdown-item" href="/blog">Blog</a></li>
+                                        <li><Link customClass="dropdown-item d-block d-sm-none" href="/" target="">Homepage</Link></li>
+                                        <li><Link customClass="dropdown-item" href="/about-me" target="">About me</Link></li>
+                                        <li><Link customClass="dropdown-item" href="/offer" target="">Offer</Link></li>
+                                        <li><Link customClass="dropdown-item" href="/contact" target="">Contact</Link></li>
+                                        <li><Link customClass="dropdown-item" href="/blog" target="">Blog</Link></li>
                                     </ul>
                                 </div>
                                 <div className="dropdown">
@@ -36,8 +34,8 @@ function Navv() {
                                         Portfolio
                                     </button>
                                     <ul className="dropdown-menu">
-                                        <li><a className="dropdown-item" href="/wedding-outdoor">Wedding outdoor</a></li>
-                                        <li><a className="dropdown-item" href="/engagement-session">Engagement session</a></li>
+                                        <li><Link customClass="dropdown-item" href="/wedding-outdoor" target="">Wedding outdoor</Link></li>
+                                        <li><Link customClass="dropdown-item" href="/engagement-session" target="">Engagement session</Link></li>
                                     </ul>
                                 </div>
                             </div>
